b30d73a458b49c8144462e7e4e675984b01168e9
Bareha <161418672+Bareha@users.noreply.github.com>
2024-06-15 20:39:06 +0530

    ControllerMain.java

--- ControllerMain.java
+++ ControllerMain.java
@@ -1,15 +1,19 @@
 package application;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ControllerMain {
+public class ControllerMain implements Initializable{
 	
 	@FXML
 	public TextField taskTitle;
@@ -17,11 +21,12 @@ public class ControllerMain {
 	public TextArea taskDescription;
 	@FXML
 	public Slider prioritySlider;
+	@FXML
+	public ListView<String> myListView;
 	
 	private List<Task> tasks = new ArrayList<>();
-	String category;
-	
-	int i = 0;
+	private List<String> taskList = new ArrayList<>();
+	String category = "all";
 	
 	public void createTask(){
 		String title = taskTitle.getText();
@@ -29,11 +34,19 @@ public class ControllerMain {
 		float priority = (float) prioritySlider.getValue();
 		boolean isCompleted = false;
 		
+		taskList.add(title);
+		
 		Task task = new Task(title, description, priority, category, isCompleted);
 		
 		tasks.add(task);
 		System.out.println(task);
+		initializeTaskListView();
+		
+		taskTitle.clear();
+		taskDescription.clear();
 	}
+	
+	
 	
 	public void selectWork(ActionEvent e) {
 		category = "work";
@@ -56,4 +69,17 @@ public class ControllerMain {
 	public void selectHome(ActionEvent e) {
 		category = "home";
 	}
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeTaskListView();
+        prioritySlider.setValue(50);
+    }
+
+    private void initializeTaskListView() {
+    	myListView.getItems().clear();
+        myListView.getItems().addAll(taskList);
+    }
 }
